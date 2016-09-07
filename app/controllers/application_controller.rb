d757b848144be31af4f06285c143cbc7df61d3b4
Omurbek Choibekov <omka3s@gmail.com>
2016-09-07 15:46:59 +0600

    third commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,23 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+def access_denied(exception)
+    flash[:danger] = exception.message
+    redirect_to root_url
+  end
+
+
+
 private
   
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) << :name,:phone,:adres
+    devise_parameter_sanitizer.permit(:sign_up) << :name
+    devise_parameter_sanitizer.permit(:sign_up) << :phone
+    devise_parameter_sanitizer.permit(:sign_up) << :adres
+    devise_parameter_sanitizer.permit(:account_update) << :name
+    devise_parameter_sanitizer.permit(:account_update) << :adres
+    devise_parameter_sanitizer.permit(:account_update) << :phone
   end
 
 end
